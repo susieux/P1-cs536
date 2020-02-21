88ab83d98c6178dcdf7114a31a22b6f998e7df12
S <24953710+susieux@users.noreply.github.com>
2020-02-21 15:54:31 -0600

    Update Sym.java

--- src/Sym.java
+++ src/Sym.java
@@ -1,15 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-//
-// Main Class File:  P1.java
-// File:             Sym.java
-// Semester:         CS536 Fall 2019
-//
-// Author:           Susie Chongthaweephol, chongthaweep@wisc.edu
-// CS Login:         susie
-// Lecturer's Name:  Aws Albarghouthi
-//
-//////////////////////////// 80 columns wide //////////////////////////////////
-
 /**
  * This class implements the Sym constructor and simple public methods-
  * getType() and toString().
