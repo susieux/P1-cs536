4778c6c9c2e7fdf439fb04226acdd3c95610e215
S <24953710+susieux@users.noreply.github.com>
2020-02-21 15:54:01 -0600

    Update EmptySymTableException.java

--- src/EmptySymTableException.java
+++ src/EmptySymTableException.java
@@ -1,15 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-//
-// Main Class File:  P1.java
-// File:             EmptySymTableException.java
-// Semester:         CS536 Fall 2019
-//
-// Author:           Susie Chongthaweephol, chongthaweep@wisc.edu
-// CS Login:         susie
-// Lecturer's Name:  Aws Albarghouthi
-//
-//////////////////////////// 80 columns wide //////////////////////////////////
-
 /**
  * This class currently just defines a checked exception thrown by the SymTable
  * class.
